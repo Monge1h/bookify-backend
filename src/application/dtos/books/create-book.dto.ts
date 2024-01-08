8efc601dbded0230db9a6464405b9b49e8afa5c9
Jorge Monge <jlo4321jl@gmail.com>
2024-01-08 14:44:06 -0600

    🔒️ Add class valitor

--- src/application/dtos/books/create-book.dto.ts
+++ src/application/dtos/books/create-book.dto.ts
@@ -1,12 +1,36 @@
-// TODO add class validator
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 export class CreateBookDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   author: string;
+
+  @IsString()
+  @IsOptional()
   isbn?: string;
+
+  @IsOptional()
   userId: number;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsString()
+  @IsOptional()
   image?: string;
+
+  @IsString()
+  @IsOptional()
   year?: string;
+
+  @IsString()
+  @IsOptional()
   genre?: string;
+
+  @IsString()
   externalId?: string;
 }
